304cd7c6bf39d01a5ac547a22e4e6f2d0727a2fe
John Dyer <johntdyer@gmail.com>
2023-07-31 19:26:17 -0400

    Update const.py

--- custom_components/promql/const.py
+++ custom_components/promql/const.py
@@ -7,7 +7,7 @@ DOMAIN: Final = "promql-fork"
 CONF_QUERY = "query"
 
 DEFAULT_NAME = "Prometheus"
-DEFAULT_HOST = "http://localhost:9090"
+DEFAULT_HOST = "http://192.168.120.9:9090"
 DEFAULT_VERIFY_SSL = False
 
 PROMETHEUS_API_PATH = "/api/v1/query"
